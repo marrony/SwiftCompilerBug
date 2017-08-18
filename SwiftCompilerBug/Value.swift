eb6bab84af802a30d11429a97dd3aaa96e909364
Marrony Neris <neris.marrony@gmail.com>
2017-08-17 23:22:11 -0300

    wip

--- SwiftCompilerBug/Value.swift
+++ SwiftCompilerBug/Value.swift
@@ -1,49 +1,10 @@
 import Foundation
 
-public protocol Value {}
-
-public protocol ScalarValue: Value, Equatable {
-    associatedtype Wrapped
-
-    var value: Wrapped { get }
-}
-
-public func ==<S: ScalarValue>(lhs: S, rhs: S) -> Bool where S.Wrapped: Equatable {
-    return lhs.value == rhs.value
-}
-
-public struct StringV: ScalarValue, CustomStringConvertible {
-    public var value: String
-
-    public init(_ value: String) {
-        self.value = value
-    }
-
-    public var description: String {
-        return "StringV(\(value))"
-    }
-}
-
-public struct IntV: ScalarValue, CustomStringConvertible {
-    public var value: Int
-
-    public init(_ value: Int) {
-        self.value = value
-    }
-
-    public var description: String {
-        return "IntV(\(value))"
-    }
-}
-
-public func != (left: Value, right: Value) -> Bool {
-    return !(left == right)
-}
-
-public func == (left: Value, right: Value) -> Bool {
+public func switchCase(_ left: Any, _ right: Any) -> Bool {
     switch (left, right) {
-    case (let lhs, let rhs) as (StringV, StringV): return lhs == rhs
-    case (let lhs, let rhs) as (IntV, IntV):       return lhs == rhs
-    default: return false
+    case (let lhs, let rhs) as (String, String):
+        return lhs == rhs
+    default:
+        return false
     }
 }
--- SwiftCompilerBugTests/SwiftCompilerBugTests.swift
+++ SwiftCompilerBugTests/SwiftCompilerBugTests.swift
@@ -4,15 +4,8 @@ import XCTest
 
 class SwiftCompilerBugTests: XCTestCase {
 
-    func testEquality1() {
-        let a: Value = IntV(10)
-        let b: Value = IntV(10)
-        XCTAssertTrue(a == b, "\(a) == \(b)")
+    func testSwitchCase() {
+        XCTAssertTrue(switchCase("hello", "hello"))
     }
 
-    func testEquality2() {
-        let a: Value = IntV(10)
-        let b: Value = IntV(20)
-        XCTAssertTrue(a != b, "\(a) != \(b)")
-    }
 }
